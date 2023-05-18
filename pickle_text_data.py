aced569b72c187bfad4a533b4db80050ea2175e0
Radoslav Ralev <ralev@in.tum.de>
2023-05-18 19:40:51 +0200

    delete a lot

--- pickle_text_data.py
+++
@@ -1,8 +0,0 @@
-import sys
-from utils import TextFilePreprocessing
-
-src_data_path = sys.argv[1]
-out_data_path = sys.argv[2]
-
-tfp = TextFilePreprocessing(filepath_in=src_data_path, filepath_out=out_data_path)
-tfp.preprocess()
