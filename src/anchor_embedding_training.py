368192665213621a75f375b76bb69a10ec730e76
Radoslav Ralev <ralev@in.tum.de>
2023-06-15 14:30:52 +0200

    code style

--- src/anchor_embedding_training.py
+++ src/anchor_embedding_training.py
@@ -1,9 +1,7 @@
+import argparse
 from tqdm import tqdm
-import sys
 import os
-
-from gensim.models import Word2Vec, FastText, KeyedVectors
-from gensim.test.utils import datapath
+from gensim.models import Word2Vec, KeyedVectors
 import multiprocessing
 import pandas as pd
 import numpy as np
@@ -11,30 +9,12 @@ from collections import defaultdict
 from unidecode import unidecode
 from utils import SentenceIterator
 
-"""
-This script is an example on how to utilize the gensim library to train anchor-embeddings.
-Usage: Provide the source embedding for the anchor vectors under en_embedding_path
-Set variables fixed and combined_vectors to the mode you want to train in.
-
-Call the script as python anchor_embedding_training.py DICTIONARY_PATH TRAINING_DATA_PATH SAVE_PATH NUM_ANCHORS EN_EMBEDDING_PATH
-where the three first arguments are files for your bilingual dictionary, training data and output file,
-while NUM_ANCHORS is an integer representing the max number of anchor vectors.
-"""
-
-cwd = os.getcwd()  # Get the current working directory (cwd)
-files = os.listdir(cwd)  # Get all the files in that directory
-os.chdir(cwd)
-
-sys.path.append('gensim/')
-# print("Files in %r: %s" % (os.getcwd(), files))
-
 
 def get_dictionary_entries(bi_dict, vecs, fixed=True, n=0):
     train_dict = dict()
     train_dict['__fix_training'] = fixed
     train_dict['__fix_init'] = True
     if n == 0:
-        # return full dict
         limit = len(bi_dict)
     else:
         limit = n
@@ -57,7 +37,6 @@ def get_combined_vectors_from_dict(bi_dict, vecs, fixed=True, n=0):
     else:
         limit = n
     for i, row in bi_dict.iterrows():
-
         decoded_de = str(row['tgt'])
         decoded_en = str(row['src'])
         if decoded_en not in vecs:
@@ -72,51 +51,70 @@ def get_combined_vectors_from_dict(bi_dict, vecs, fixed=True, n=0):
     combined_dict['__fix_init'] = True
     for entry, vectors in train_dict.items():
         combined_dict[entry] = np.mean(vectors, axis=0)
-
     return combined_dict
 
 
-fixed = True
-combined_vectors = True
-print("Train embeddings start.")
-# "data/lexicons/mk-en_lexicon_no_test.txt"
-bi_dict_path = os.path.abspath(cwd+'/'+sys.argv[1])
-# "data/macedonian/mkwiki.full"
-tgt_data_path = os.path.abspath(cwd+'/'+sys.argv[2])
-# "data/macedonian/embeddings/W2V_MK_300D_CBOW_aligned.kv"
-tgt_embedding_path = os.path.abspath(cwd+'/'+sys.argv[3])
-limit = int(sys.argv[4])
-# "data/embeddings/EN_300D_CBOW.kv"
-src_embedding_path = os.path.abspath(cwd+'/'+sys.argv[5])
-train_type = 1 if sys.argv[6].lower().strip() == 'sg' else 0
-size = int(sys.argv[7])
-max_final_vocab = int(sys.argv[8])
+def main():
+    parser = argparse.ArgumentParser(
+        description='Train anchor-embeddings using gensim library.')
+    parser.add_argument(
+        'bi_dict_path', help='Path to your bilingual dictionary.')
+    parser.add_argument('tgt_data_path', help='Path to your training data.')
+    parser.add_argument('tgt_embedding_path', help='Path to your output file.')
+    parser.add_argument('limit', type=int,
+                        help='Max number of anchor vectors.')
+    parser.add_argument(
+        'src_embedding_path', help='Path to the source embedding for the anchor vectors.')
+    parser.add_argument(
+        'train_type', help='Type of training. Use "sg" for skip-gram, anything else for CBOW.')
+    parser.add_argument('size', type=int, help='Dimension of the vectors.')
+    parser.add_argument('max_final_vocab', type=int,
+                        help='Maximum final vocabulary size.')
+    args = parser.parse_args()
 
-training_corpus = SentenceIterator(tgt_data_path)
+    cwd = os.getcwd()  # Get the current working directory (cwd)
+    os.chdir(cwd)
+
+    fixed = True
+    combined_vectors = True
+    print("Train embeddings start.")
+
+    bi_dict_path = os.path.abspath(cwd+'/'+args.bi_dict_path)
+    tgt_data_path = os.path.abspath(cwd+'/'+args.tgt_data_path)
+    tgt_embedding_path = os.path.abspath(cwd+'/'+args.tgt_embedding_path)
+    limit = args.limit
+    src_embedding_path = os.path.abspath(cwd+'/'+args.src_embedding_path)
+    train_type = 1 if args.train_type.lower().strip() == 'sg' else 0
+    size = args.size
+    max_final_vocab = args.max_final_vocab
+
+    training_corpus = SentenceIterator(tgt_data_path)
 # Load a word2vec model stored in the C *text* format.
-vectors_src = KeyedVectors.load_word2vec_format(
-    datapath(src_embedding_path), binary=False)
+    vectors_src = KeyedVectors.load_word2vec_format(
+        src_embedding_path, binary=False)
 # vectors_src = KeyedVectors.load(src_embedding_path, mmap='r')
 
-colnames = ['src', 'tgt']
-bi_dict = pd.read_csv(bi_dict_path, names=colnames, sep=' ')
-if combined_vectors:
-    train_dict = get_combined_vectors_from_dict(
-        bi_dict, vecs=vectors_src, fixed=fixed, n=limit)
-    # save the anchor words. Maybe to a txt file
-    anchor_filename = f"data/anchor_words_{limit}_{sys.argv[6].lower().strip()}.txt"
-    with open(anchor_filename, 'w+') as file_anchors:
-        for anchor_word in tqdm(list(train_dict.keys())):
-            file_anchors.write(anchor_word+'\n')
-else:
-    train_dict = get_dictionary_entries(
-        bi_dict, vecs=vectors_src, fixed=fixed, n=limit)
+    colnames = ['src', 'tgt']
+    bi_dict = pd.read_csv(bi_dict_path, names=colnames, sep=' ')
+    if combined_vectors:
+        train_dict = get_combined_vectors_from_dict(
+            bi_dict, vecs=vectors_src, fixed=fixed, n=limit)
+        anchor_filename = f"data/anchor_words_{limit}_{args.train_type.lower().strip()}.txt"
+        with open(anchor_filename, 'w+') as file_anchors:
+            for anchor_word in tqdm(list(train_dict.keys())):
+                file_anchors.write(anchor_word+'\n')
+    else:
+        train_dict = get_dictionary_entries(
+            bi_dict, vecs=vectors_src, fixed=fixed, n=limit)
 
-# TO DO: Change size to 300
-model = Word2Vec(training_corpus, vector_size=size, window=5, min_count=3,
-                 workers=multiprocessing.cpu_count(), sg=train_type, epochs=5,
-                 fixed_vectors=train_dict, max_final_vocab=max_final_vocab)
+    model = Word2Vec(training_corpus, vector_size=size, window=5, min_count=3,
+                     workers=multiprocessing.cpu_count(), sg=train_type, epochs=5,
+                     fixed_vectors=train_dict, max_final_vocab=max_final_vocab)
 
-print("Model finished, saving vectors...")
-model.wv.save_word2vec_format(tgt_embedding_path)
-print("Done.")
+    print("Model finished, saving vectors...")
+    model.wv.save_word2vec_format(tgt_embedding_path)
+    print("Done.")
+
+
+if __name__ == "__main__":
+    main()
--- src/concat_kv.py
+++ src/concat_kv.py
@@ -1,35 +1,43 @@
-from gensim.models import KeyedVectors
-import sys 
+import argparse
 from tqdm import tqdm
 
-src_path = sys.argv[1]
-src2_path = sys.argv[2] 
-word_len = 2
 
-f = open(src2_path, encoding="utf8")
-file_data = f.readlines()
-header = file_data[0]
-file_data = file_data[1:]
+def concatenate_files(src1_path, src2_path, output_path):
+    with open(src2_path, encoding="utf8") as f:
+        file_data = f.readlines()
+    header = file_data[0]
+    file_data = file_data[1:]
 
-old_file_data = open(src_path, encoding='utf8').readlines()
-header_old = old_file_data[0]
-old_file_data = old_file_data[1:]
+    with open(src1_path, encoding='utf8') as f:
+        old_file_data = f.readlines()
+    header_old = old_file_data[0]
+    old_file_data = old_file_data[1:]
 
-extract_arg = lambda x, y: int(x.split(' ')[y])
+    def extract_arg(x, y): return int(x.split(' ')[y])
 
-total_length = extract_arg(header, 0) + extract_arg(header_old, 0)
-vec_size = extract_arg(header, 1)
+    total_length = extract_arg(header, 0) + extract_arg(header_old, 0)
+    vec_size = extract_arg(header, 1)
 
-with open(src_path, 'w', encoding='utf-8') as out_f:
-    out_f.write(f'{total_length} {vec_size}\n')
-    for s in tqdm(old_file_data):
-        out_f.write(s)
+    with open(output_path, 'w', encoding='utf-8') as out_f:
+        out_f.write(f'{total_length} {vec_size}\n')
+        for s in tqdm(old_file_data):
+            out_f.write(s)
 
-out_f.close()
-
-with open(src_path, 'a', encoding='utf-8') as out_f:
-    for s in tqdm(file_data):
-        out_f.write(s)
+    with open(output_path, 'a', encoding='utf-8') as out_f:
+        for s in tqdm(file_data):
+            out_f.write(s)
 
 
-f.close()
+def main():
+    parser = argparse.ArgumentParser(
+        description='Concatenate two KeyedVectors files.')
+    parser.add_argument('src1_path', help='Path to the first source file.')
+    parser.add_argument('src2_path', help='Path to the second source file.')
+    parser.add_argument('output_path', help='Path to the output file.')
+    args = parser.parse_args()
+
+    concatenate_files(args.src1_path, args.src2_path, args.output_path)
+
+
+if __name__ == "__main__":
+    main()
